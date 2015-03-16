0398986549cc7a44284dce71c6f0a01ac339bf3d
jsanto <jsanto@psych-0574.local>
2015-03-16 13:36:13 -0400

    Initial Commit

---
+++ JJSAssignement4.7/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JJSAssignement4.7
+//
+//  Created by jsanto on 3/16/15.
+//  Copyright (c) 2015 Jason Santo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JJSAssignement4.7/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JJSAssignement4.7
+//
+//  Created by jsanto on 3/16/15.
+//  Copyright (c) 2015 Jason Santo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
